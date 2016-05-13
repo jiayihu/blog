c7ab0e489a8ab7ce4f7399a41c2f1234485e8480
Jiayi Hu <steph.jiayi@gmail.com>
2016-05-13 18:42:23 +0200

    Set backbone blog

--- index.js
+++ index.js
@@ -1,6 +1,40 @@
-import metalsmith from 'metalsmith';
+const metalsmith = require('metalsmith');
+const md = require('metalsmith-markdown');
+const layouts = require('metalsmith-layouts');
+const permalinks = require('metalsmith-permalinks');
+const collections = require('metalsmith-collections');
+const pagination  = require('metalsmith-pagination');
+const snippet     = require('metalsmith-snippet');
 
 metalsmith(__dirname)
   .source('src')
-  .destination('build')
+  .use(collections({
+    articles: {
+      pattern: 'articles/**/*.md',
+      sortBy: 'date',
+      reverse: true
+    }
+  }))
+  .use(pagination({
+    'collections.articles': {
+      perPage: 5,
+      first: 'index.html',
+      path: 'page/:num/index.html',
+      layout: 'index.jade'
+    }
+  }))
+  .use(md({
+    gfm: true
+  }))
+  .use(snippet({
+    maxLength: 300
+  }))
+  .use(permalinks({
+    pattern: ':title'
+  }))
+  .use(layouts({
+    engine: 'jade',
+    pretty: true
+  }))
+  .destination('public')
   .build((err) => console.log(err));
---
+++ src/articles/example.md
@@ -0,0 +1,8 @@
+---
+title: Example title.
+date: 2014-09-29 23:00
+author: Blake Embrey
+layout: article.jade
+---
+
+Example content. Meh.
