d8d06186e8bfb3f8113ff483dfd93d72ebf84f50
Jiayi Hu <jiayi.ghu@gmail.com>
2017-07-20 22:31:15 +0200

    feat: use nunjucks and support drafts

--- index.js
+++ index.js
@@ -1,40 +1,55 @@
 const metalsmith = require('metalsmith');
 const md = require('metalsmith-markdown');
 const layouts = require('metalsmith-layouts');
-const permalinks = require('metalsmith-permalinks');
 const collections = require('metalsmith-collections');
-const pagination  = require('metalsmith-pagination');
-const snippet     = require('metalsmith-snippet');
+const drafts = require('metalsmith-drafts');
+const permalinks = require('metalsmith-permalinks');
+const pagination = require('metalsmith-pagination');
+const snippet = require('metalsmith-snippet');
 
 metalsmith(__dirname)
   .source('src')
-  .use(collections({
-    articles: {
-      pattern: 'articles/**/*.md',
-      sortBy: 'date',
-      reverse: true
-    }
-  }))
-  .use(pagination({
-    'collections.articles': {
-      perPage: 5,
-      first: 'index.html',
-      path: 'page/:num/index.html',
-      layout: 'index.jade'
-    }
-  }))
-  .use(md({
-    gfm: true
-  }))
-  .use(snippet({
-    maxLength: 300
-  }))
-  .use(permalinks({
-    pattern: ':title'
-  }))
-  .use(layouts({
-    engine: 'jade',
-    pretty: true
-  }))
+  .clean(true)
+  .use(
+    collections({
+      articles: {
+        pattern: 'articles/**/*.md',
+        sortBy: 'date',
+        reverse: true,
+      },
+    })
+  )
+  .use(drafts())
+  .use(
+    pagination({
+      'collections.articles': {
+        perPage: 5,
+        first: 'index.html',
+        path: 'page/:num/index.html',
+        layout: 'index.html',
+      },
+    })
+  )
+  .use(
+    md({
+      gfm: true,
+    })
+  )
+  .use(
+    snippet({
+      maxLength: 300,
+    })
+  )
+  .use(
+    permalinks({
+      pattern: ':title',
+    })
+  )
+  .use(
+    layouts({
+      engine: 'nunjucks',
+      pretty: true,
+    })
+  )
   .destination('public')
-  .build((err) => console.log(err));
+  .build(err => console.log(err));
--- src/articles/draft.md
+++ src/articles/draft.md
@@ -3,6 +3,7 @@ title: Example title.
 date: 2014-09-29 23:00
 author: Blake Embrey
 layout: article.html
+draft: true
 ---
 
 Example content. Meh.
