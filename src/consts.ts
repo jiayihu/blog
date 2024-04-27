463b3d15272da7659698f4fbfa9eecec1e6ec811
Jiayi Hu <jiayi.ghu@gmail.com>
2024-04-27 14:32:12 +0200

    Minor fixes

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Jiayi's Chronicles";
-export const SITE_DESCRIPTION = 'Chronicles of a young developer';
+export const SITE_DESCRIPTION = "Chronicles of a web developer";
