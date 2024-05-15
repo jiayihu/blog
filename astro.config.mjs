a7a528564010ef131c3883ae214f4d9a65f4ffc3
Jiayi Hu <jiayi.ghu@gmail.com>
2024-05-15 21:54:06 +0200

    Fnish improving the article

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import mdx from "@astrojs/mdx";
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
+
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog.jiayihu.net/",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
   markdown: {
     shikiConfig: {
-      theme: "github-light",
-    },
-  },
+      theme: "github-light"
+    }
+  }
 });
